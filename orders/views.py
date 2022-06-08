fa18ac89466f0219162ee91b503a39188005f977
unknown <gchukwuma@oldmutualnigeria.com>
2022-06-08 15:34:33 +0100

    (Payment gateway, After order and rating functionalities added)

--- orders/views.py
+++ orders/views.py
@@ -1,15 +1,86 @@
 import datetime
+from email import message
 from time import strftime
-from django.http import HttpResponse
+from django.http import HttpResponse, JsonResponse
 from django.shortcuts import redirect, render
+import json
 from carts.models import CartItem
-from orders.models import Order
+from orders.models import Order, OrderProduct, Payment
+from store.models import Product
 from .forms import OrderForm
+from django.core.mail import EmailMessage
+from django.template.loader import render_to_string
 
 # Create your views here.
 
 def payments(request):
-    return render(request, "orders/payments.html")
+    body = json.loads(request.body)
+    order = Order.objects.get(user=request.user, is_ordered=False, order_number=body["orderID"])
+
+    payment = Payment(
+        user = request.user,
+        payment_id = body["transID"],
+        payment_method = body["payment_method"],
+        amount_paid = order.order_total,
+        status = body["status"],
+    )
+    payment.save()
+    order.payment = payment
+    order.is_ordered = True
+    order.save()
+
+    #Move the cart items to order product table
+    cart_items = CartItem.objects.filter(user=request.user)
+
+    for item in cart_items:
+        orderproduct = OrderProduct()
+        orderproduct.order_id = order.id
+        orderproduct.payment = payment
+        orderproduct.user_id = request.user.id
+        orderproduct.product_id =item.product_id
+        orderproduct.quantity =item.quantity
+        orderproduct.product_price =item.product.price
+        orderproduct.ordered = True
+        orderproduct.save()
+
+        cart_item = CartItem.objects.get(id=item.id)
+        product_variation = cart_item.variations.all()
+        orderproduct = OrderProduct.objects.get(id=orderproduct.id)
+        orderproduct.variation.set(product_variation)
+        orderproduct.save()
+
+        product = Product.objects.get(id=item.product_id)
+        product.stock -= item.quantity
+        product.save()
+    
+    #clear cart
+    CartItem.objects.filter(user=request.user).delete()
+
+    print(f"Payment ID - Payment Function - ZEEE{payment.payment_id}")
+
+    # mail_subject = "Thank you for your order"
+    # message = render_to_string("orders/order_received_email.html", {
+    #     'user':request.user,
+    #     'order':order,
+    # })
+
+    # to_email = request.user.email
+    # send_email = EmailMessage(mail_subject, message, to=[to_email])
+    # send_email.send()
+    print(f"Payment ID - Payment Function - 1{payment.payment_id}")
+
+    #SEND ORDER NUMBER AND TRANSACTION ID BACK TO SENDDATA METHOD VIA JSONRESPONSE
+
+    data = {
+        'order_number': order.order_number,
+        'transID': payment.payment_id,
+    }
+    paye = data['transID']
+    print(f"Payment ID - Payment Function - 2{payment.payment_id}")
+    print(f"Payment ID - Payment Function - 1{paye}")
+
+    print(data['transID'])
+    return JsonResponse(data)
 
 
 def place_order(request, total = 0, quantity = 0,):
@@ -67,6 +138,40 @@ def place_order(request, total = 0, quantity = 0,):
                 "tax":tax,
                 'grand_total':grand_total,
             }
-            return render(request, 'orders/payments.html', context=context)
+
+            return render(request, 'orders/payments.html', context)
+        else:
+            print("Form Error -", form.errors)
     else:
         return redirect('checkout')
+
+def order_complete(request):
+    print(request)
+    order_number = request.GET.get('order_number')
+    transID = request.GET.get('payment_id')
+
+    print(order_number, transID)
+
+    try:
+        order = Order.objects.get(order_number=order_number, is_ordered=True)
+        ordered_products = OrderProduct.objects.filter(order_id = order.id)
+        payment = Payment.objects.get(payment_id = transID)
+        
+
+        sub_total = 0
+
+        for i in ordered_products:
+            sub_total += i.product_price * i.quantity
+        
+        context = {
+            'order':order,
+            'ordered_products': ordered_products,
+            'order_number': order.order_number,
+            'transID':payment.payment_id,
+            'payment':payment,
+            'sub_total':sub_total,
+        }
+        return render(request, "orders/order_complete.html", context)
+    except (Payment.DoesNotExist, Order.DoesNotExist):
+
+        return redirect('home')
--- store/views.py
+++ store/views.py
@@ -1,13 +1,15 @@
 from ast import keyword
 from django.http import HttpResponse
-from django.shortcuts import get_object_or_404, render
+from django.shortcuts import get_object_or_404, redirect, render
 from carts.models import CartItem
 from carts.views import _cart_id
 from category.models import Category
 from django.db.models import Q
-
-from store.models import Product
+from store.forms import ReviewForm
+from django.contrib import messages
+from store.models import Product, ReviewRating
 from django.core.paginator import EmptyPage,PageNotAnInteger,Paginator
+from orders.models import OrderProduct
 
 # Create your views here.
 
@@ -45,10 +47,24 @@ def product_detail(request, category_slug, product_slug):
     except Exception as e:
         raise e
     
+    try:
+        orderproduct = OrderProduct.objects.filter(user=request.user, product_id = single_product.id).exists()
+    except OrderProduct.DoesNotExist:
+        orderproduct = None
+
+    #GET THE REVIEWS
+
+    reviews = ReviewRating.objects.filter(product_id=single_product.id, status=True)
+
+
+
     context = {
         'single_product':single_product,
-        'in_cart':in_cart
+        'in_cart':in_cart,
+        'orderproduct':orderproduct,
+        "reviews":reviews,
     }
+
 
     return render(request, "store/product_detail.html", context)
 
@@ -63,3 +79,26 @@ def search(request):
         "product_count": product_count,
     }
     return render(request, "store/store.html", context)
+
+def submit_review(request, product_id):
+    url = request.META.get('HTTP_REFERER')
+    if request.method == "POST":
+        try:
+            reviews = ReviewRating.objects.get(user__id = request.user.id, product__id = product_id)
+            form = ReviewForm(request.POST, instance = reviews)
+            form.save()
+            messages.success("Thank you. Your Review has been updated.")
+            return redirect(url)
+        except ReviewRating.DoesNotExist:
+            form = ReviewForm(request.POST)
+            if form.is_valid():
+                data = ReviewRating()
+                data.subject = form.cleaned_data['subject']
+                data.rating = form.cleaned_data['rating']
+                data.review = form.cleaned_data['review']
+                data.ip = request.META.get('REMOTE_ADDR')
+                data.product_id = product_id
+                data.user_id = request.user.id
+                data.save()
+                messages.success("Thank you. Your Review has been submitted.")
+                return redirect(url)
